79c3bb628bb37dabb5769346b9903c081a246e94
imranosman886_beep <imranosman886@gmail.com>
2026-09-15 12:34:41 -0700

    Add Song class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,29 @@
 class Song:
-    pass
+    count = 0
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 0
+
+        Song.genre_count[genre] += 1
+
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 0
+
+        Song.artist_count[artist] += 1
+
+    @classmethod
+    def genres(cls):
+        return cls.genre_count.keys()
+
+    @classmethod
+    def artists(cls):
+        return cls.artist_count.keys()
