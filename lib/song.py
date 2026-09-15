9bd256ee6197632e6f080b9fc862efc737cece70
imranosman886_beep <imranosman886@gmail.com>
2026-09-15 12:38:29 -0700

    Add Song class

--- lib/song.py
+++ lib/song.py
@@ -1,5 +1,7 @@
 class Song:
     count = 0
+    genres = set()
+    artists = set()
     genre_count = {}
     artist_count = {}
 
@@ -10,20 +12,13 @@ class Song:
 
         Song.count += 1
 
+        Song.genres.add(genre)
+        Song.artists.add(artist)
+
         if genre not in Song.genre_count:
             Song.genre_count[genre] = 0
-
         Song.genre_count[genre] += 1
 
         if artist not in Song.artist_count:
             Song.artist_count[artist] = 0
-
         Song.artist_count[artist] += 1
-
-    @classmethod
-    def genres(cls):
-        return cls.genre_count.keys()
-
-    @classmethod
-    def artists(cls):
-        return cls.artist_count.keys()
